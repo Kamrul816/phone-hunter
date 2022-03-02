3044d0f377be347289d3acdcb2b1b0137416f522
Kamrul816 <freelancerkamrul816@gmail.com>
2022-03-02 06:42:07 +0600

    clear data & result

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const searchPhone = () => {
     const searchfield = document.getElementById('search-field');
     const searchText = searchfield.value;
+    // clear data
     searchfield.value = '';
+    // load data
     const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(res => res.json())
@@ -10,7 +12,12 @@ const searchPhone = () => {
 
 const displaySearchResult = phones => {
     const searchResult = document.getElementById('search-result');
+    
+    // clear result
+    searchResult.innerHTML = '';
+    
     phones.forEach(phone => {
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -19,9 +26,18 @@ const displaySearchResult = phones => {
             <div class="card-body">
               <h5 class="card-title">${phone.phone_name}</h5>
               <p class="card-text">${phone.brand}</p>
+              <button onclick = "loadPhoneDetails('${phone.phone_name}')">Details Explore</button>
             </div>
         </div>
         ` ;
         searchResult.appendChild(div);
     })
+}
+
+const loadPhoneDetails = slug => {
+    const url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089${slug}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.mainFeatures));
+
 }
